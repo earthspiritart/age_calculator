892444fb9eae129a660252459ca6be37adb70cf5
David H <djh@infowest.com>
2014-03-09 07:48:37 -0700

    age calc

---
+++ age_calc.rb
@@ -0,0 +1,28 @@
+class Person
+  def initialize(age, name)
+    @name = name
+    @age = age
+  end
+  
+  def tell_about
+    if @age >= 16 && @age < 18
+      puts "#{@name} can drive"
+    elsif @age >= 18 && @age < 25
+      puts "#{@name} can vote and smoke"
+    elsif @age >= 25 && @age < 65
+      puts "#{@name} can rent a car"
+    elsif @age >= 65
+      puts "#{@name} can now retire"
+    elsif @age < 5
+      puts "#{@name} should go watch cartoons"
+    else
+      puts "sorry charlie, that don't compute"
+    end
+  end
+   
+    p = Person.new(19, "tom")
+    p.tell_about
+    p2 =Person.new(66, "frank")
+    p2.tell_about
+
+ end
